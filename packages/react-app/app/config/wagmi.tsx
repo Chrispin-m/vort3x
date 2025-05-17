0354f3012ffd784798af086cd0580d2f319b7193
Chrispin-m <mwangi.wachira2019@students.jkuat.ac.ke>
2025-05-17 05:04:27 +0300

    clean commit

--- packages/react-app/app/config/wagmi.tsx
+++ packages/react-app/app/config/wagmi.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { WagmiConfig, createConfig, http } from "wagmi";
+import { WagmiConfig, createConfig, http, fallback } from "wagmi";
 import { injected } from "wagmi/connectors";
 import { celoAlfajores, mainnet } from "wagmi/chains";
 import type { ReactNode } from "react";
@@ -9,8 +9,14 @@ const wagmiConfig = createConfig({
   chains: [celoAlfajores, mainnet],
   connectors: [injected()],
   transports: {
-    [celoAlfajores.id]: http(),
-    [mainnet.id]: http(),
+    [celoAlfajores.id]: fallback([
+      http("https://alfajores-forno.celo-testnet.org"),
+      http()
+    ]),
+    [mainnet.id]: fallback([
+      http("https://eth.llamarpc.com"),
+      http()
+    ]),
   },
 });
 
