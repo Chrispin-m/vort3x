f5bc0f29544368d1dc858af22245845950b5f52e
Chrispin-m <mwangi.wachira2019@students.jkuat.ac.ke>
2025-06-03 03:38:20 +0300

    new va

--- packages/react-app/app/config/addresses.ts
+++ packages/react-app/app/config/addresses.ts
@@ -1,2 +1,2 @@
 export const cusdContractAddress = "0x2F25deB3848C207fc8E0c34035B3Ba7fC157602B";
-export const VortexAddress     = "0x628F1A36E2E4fa1ed9CD91586c44e6829b369497";
+export const VortexAddress     = "0xdeAFa17D50dBa6224177FFA396395A7E096f250E";
