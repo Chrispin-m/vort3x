b8e320e4723ea7261f9df00fff144065f56ce01a
Chrispin-m <mwangi.wachira2019@students.jkuat.ac.ke>
2025-06-03 04:35:56 +0300

    look and feel

--- packages/react-app/components/Footer.tsx
+++ packages/react-app/components/Footer.tsx
@@ -31,6 +31,7 @@ export default function Footer() {
   return (
     <footer className="bg-gypsum border-black border-t py-4 w-full">
       <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="flex justify-center space-x-6 md:order-2">
           {navigation.map((item) => (
             <a
               key={item.name}
