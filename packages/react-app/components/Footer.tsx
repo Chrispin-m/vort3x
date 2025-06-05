b6f22d3d19af9630d281ed559528438c43cffe44
Chrispin-m <mwangi.wachira2019@students.jkuat.ac.ke>
2025-06-05 06:18:17 +0300

    footer lk

--- packages/react-app/components/Footer.tsx
+++ packages/react-app/components/Footer.tsx
@@ -9,6 +9,18 @@ const navigation = [
     name: "Twitter",
     href: "https://twitter.com/CeloDevs",
     icon: (props: Props) => (
+      <svg
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        {...props}
+        style={{
+          width: "1.5rem",
+          height: "1.5rem",
+          display: "block",
+        }}
+      >
+        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+      </svg>
     ),
   },
 ];
@@ -95,38 +107,8 @@ export default function Footer() {
             position: "relative",
           }}
         >
+          {/*//NT */}
           <div style={{ display: "flex", gap: "1rem", order: 2 }}>
-            {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
-                  position: "relative",
-                  zIndex: 31,
-                  color: "rgba(184,242,254,1)",
-                  display: "inline-block",
-                  transition: "transform 0.3s",
-                  animation: "float 6s ease-in-out infinite",
-                }}
-              >
-                <span
-                  style={{
-                    position: "absolute",
-                    width: 1,
-                    height: 1,
-                    padding: 0,
-                    overflow: "hidden",
-                    clip: "rect(0,0,0,0)",
-                    border: 0,
-                  }}
-                >
-                  {item.name}
-                </span>
-                <item.icon />
-              </a>
-            ))}
           </div>
 
           <div
@@ -206,7 +188,7 @@ export default function Footer() {
         rel="noopener noreferrer"
         style={{
           position: "fixed",
-          bottom: "4rem",  
+          bottom: "4rem", 
           right: "2rem",
           zIndex: 40,
           width: "3.5rem",
