8365bdc7a753bf74e71aa34015beb16b59285748
Chrispin-m <mwangi.wachira2019@students.jkuat.ac.ke>
2025-06-06 04:17:54 +0300

    fconnectors config

--- packages/react-app/app/config/wagmi.tsx
+++ packages/react-app/app/config/wagmi.tsx
@@ -1,26 +1,62 @@
 "use client";
 
 import { WagmiConfig, createConfig, http, fallback } from "wagmi";
-import { injected } from "wagmi/connectors";
 import { celoAlfajores, mainnet } from "wagmi/chains";
-import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, getDefaultWallets, darkTheme } from "@rainbow-me/rainbowkit";
+import { SafeConnector } from "wagmi/connectors/safe";
 import type { ReactNode } from "react";
 
 const WC_PROJECT_ID = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
-if (!WC_PROJECT_ID) {
-  throw new Error("WC_PROJECT_ID is missing");
-}
+if (!WC_PROJECT_ID) throw new Error("WC_PROJECT_ID is missing");
+
+const { wallets: defaultWallets } = getDefaultWallets({
+  appName: "Vort3x",
+  projectId: WC_PROJECT_ID,
+  chains: [celoAlfajores, mainnet],
+});
+
+// custom wallets configuration
+const wallets = [
+  ...defaultWallets,
+  {
+    groupName: "Smart Wallets",
+    wallets: [
+      {
+        id: "safe",
+        name: "Safe",
+        iconUrl: "https://safe.global/images/logo.svg",
+        iconBackground: "#ffffff",
+        createConnector: () => ({
+          connector: new SafeConnector({
+            chains: [celoAlfajores, mainnet],
+            options: {
+              allowedDomains: [/app.safe.global$/],
+              debug: false,
+            },
+          }),
+        }),
+      }
+    ],
+  },
+];
 
 const { connectors } = getDefaultWallets({
   appName: "Vort3x",
   projectId: WC_PROJECT_ID,
+  wallets,
 });
 
 const wagmiConfig = createConfig({
-  chains: [celoAlfajores, mainnet],
+  autoConnect: true,
   connectors: [
-    injected(), 
     ...connectors,
+    new SafeConnector({ 
+      chains: [celoAlfajores, mainnet],
+      options: {
+        allowedDomains: [/app.safe.global$/],
+        debug: false,
+      },
+    }),
   ],
   transports: {
     [celoAlfajores.id]: fallback([
@@ -37,7 +73,11 @@ const wagmiConfig = createConfig({
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider>  {/* */}
+      <RainbowKitProvider 
+        theme={darkTheme()}
+        modalSize="compact"
+        wallets={wallets}
+      >
         {children}
       </RainbowKitProvider>
     </WagmiConfig>
